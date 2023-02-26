fa9c4067c124844f55d24b2fe86f31a4a981fa41
Aditya <adityagundg14@gmail.com>
2023-02-26 09:45:47 +0530

    Fourth commit

--- Control Statement Programs/src/Looping/ArmstrongNumber.java
+++ Control Statement Programs/src/Looping/ArmstrongNumber.java
@@ -1,9 +1,27 @@
 package Looping;
 
+import java.util.Scanner;
+
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num=sc.nextInt();
+		int temp=num;
+		int sum=0;
+		while(num>0) {
+			int lastdigit=num%10;
+			sum=sum+(lastdigit*lastdigit*lastdigit);
+			num=num/10;
+		}
+		if(sum==temp) {
+			System.out.println("It is a Armstrong Number");
+		}
+		else {
+			System.out.println("Not a Armstrong Number");
+		}
+
 	}
 
 }
